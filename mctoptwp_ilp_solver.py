3f82aa0f029ae480b788611bc0fb4dbb86b72077
ksylejmani <kadri.sylejmani@uni-pr.edu>
2023-03-25 23:48:32 +0100

    first commit

---
+++ mctoptwp_ilp_solver.py
@@ -0,0 +1,21 @@
+class MCTOPTWP_ILP:
+    def __init__(self) -> None:
+        """Initiate object"""
+        pass
+
+    def parse_instance(self):
+        """Read data from file"""
+        pass
+
+    def create_model(self, computation_time):
+        """"Create Google OR-Tools"""
+        pass
+
+
+
+if __name__=="__main__":
+    """Create an object and use parameters"""
+    instance_name=""
+    computation_time=300
+
+    pass
